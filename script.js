fff2cbd3af046bdf1f1fb467d9695bef9bdbce0c
farhan ibne saif <farhanibne860@gmail.com>
2023-01-26 08:10:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var doge = document.getElementById("doge");
 var sol = document.getElementById("solana");
  
 const API_URL =
-"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Clitecoin%2Cethereum%2Cdogecoin%2Csolana&vs_currencies=usd"
+"#######################################################"
  
 fetch(API_URL)
     .then(response => response.json())
